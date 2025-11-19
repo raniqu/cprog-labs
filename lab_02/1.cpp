8662d3993d85d2b702e4d14bd90b50264b8218e3
Arina <insidetheworldcompany@gmail.com>
2025-11-19 10:03:14 +0300

    Lab_02 fixed in task1

--- lab_02/1.cpp
+++ lab_02/1.cpp
@@ -4,16 +4,46 @@ int main() {
     int x, y, z;
     std::cin >> x >> y >> z;
     bool a1 = ((x%2 != 0) && (y%2 != 0));
-    bool a2 = (((x < 20) + (y < 20)) == 1);
-    bool a3 = ((x == 0) || (y == 0));
-    bool a4 = ((x < 0) && (y < 0) && (z < 0));
-    bool a5 = (((x %5 == 0) + (y % 5 == 0) + (z % 5 == 0)) == 1);
-    bool a6 = ((x > 100) || (y > 100) || (z > 100));
-    if ((a1 + a2 + a3 + a4 + a5 + a6) == 6) {
+    if (a1 == 1) {
         std::cout << "condition is true" << std::endl;
     }
     else {
         std::cout << "condition is false" << std::endl;
     }
 
+    bool a2 = (((x < 20) + (y < 20)) == 1);
+    if (a2 == 1) {
+        std::cout << "condition is true" << std::endl;
+    }
+    else {
+        std::cout << "condition is false" << std::endl;
+    }
+    bool a3 = ((x == 0) || (y == 0));
+    if (a3 == 1) {
+        std::cout << "condition is true" << std::endl;
+    }
+    else {
+        std::cout << "condition is false" << std::endl;
+    }
+    bool a4 = ((x < 0) && (y < 0) && (z < 0));
+    if (a4 == 1) {
+        std::cout << "condition is true" << std::endl;
+    }
+    else {
+        std::cout << "condition is false" << std::endl;
+    }
+    bool a5 = (((x %5 == 0) + (y % 5 == 0) + (z % 5 == 0)) == 1);
+    if (a5 == 1) {
+        std::cout << "condition is true" << std::endl;
+    }
+    else {
+        std::cout << "condition is false" << std::endl;
+    }
+    bool a6 = ((x > 100) || (y > 100) || (z > 100));
+    if (a6 == 1) {
+        std::cout << "condition is true" << std::endl;
+    }
+    else {
+        std::cout << "condition is false" << std::endl;
+    }
 }
