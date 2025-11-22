1738ce561c79062f56e50d03b47063cfec2115e7
Arina <insidetheworldcompany@gmail.com>
2025-11-22 20:40:53 +0300

    task2 Lab_02 added solution

--- lab_02/2.cpp
+++ lab_02/2.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 
+using byte = unsigned char;
+
+bool isSameCell(std::pair<byte, byte> a, std::pair<byte, byte> b) {
+	return (a.first == b.first) && (a.second == b.second);
+}
 int main() {
-    std::pair<int, int> position = {0, 4};
-    // std::cin >> position.first >> position.second;
-    std::cout << position.first << position.second<<std::endl;
+
+	std::pair<byte, byte> A, B;
+	std::cout << "enter coordinates positionA" << std::endl;
+	std::cin >> A.first >> A.second;
+	std::cout << "enter coordinates positionB" << std::endl;
+	std::cin >> B.first >> B.second;
+	if (!isSameCell(A, B)) {
+
+		if ((A.first == B.first) || (A.second == B.second)) {
+			std::cout << "The rook is threatening" << std::endl;
+
+		}
+		if (abs(A.first - B.first) == abs(A.second - B.second)) {
+			std::cout << "The bishop is threatening" << std::endl;
+		}
+
+		if ((abs(A.first - B.first) <= 1) && (abs(A.second - B.second) <= 1)) {
+			std::cout << "The king can move to square B" << std::endl;
+		}
+
+		if (((A.first == B.first) || (A.second == B.second)) || (abs(A.first - B.first) == abs(A.second - B.second))) {
+			std::cout << "The queen is threatening" << std::endl;
+		}
+
+		if ((A.first == B.first) && (A.second + 1 == B.second)) {
+			std::cout << "usual pawn move" << std::endl;
+		}
+
+		if ((abs(A.first - B.first) == 1) && (A.second + 1 == B.second)) {
+			std::cout << "pawn move when taking a piece" << std::endl;
+		}
+	}
+
 }
--- lab_02/4.cpp
+++ lab_02/4.cpp
@@ -5,29 +5,32 @@ int main() {
     for (int i{8}; i < 16; i++) {
         pr *= i;
     }
-    std::cout << pr << std::endl;
+    std::cout << "from 8 to 15 " << pr << std::endl;
 
     int a;
+    std::cout << "enter a " << std::endl;
     std::cin >> a;
     pr = 1;
     for (int i{a}; i < 21; i++) {
         pr *= i;
     }
-    std::cout << pr << std::endl;
+    std::cout << "from a to 20 " << pr << std::endl;
 
     int b;
+    std::cout << "enter b, but b >= a" << std::endl;
     std::cin >> b;
 
     pr = 1;
     for (int i{1}; i < (b+1); i++) {
         pr *= i;
     }
-    std::cout << pr << std::endl;
-
-    pr = 1;
-    for (int i{a}; i < (b+1); i++) {
-        pr *= i;
+    std::cout << "from 1 to b " << pr << std::endl;
+    if (a <= b){
+        pr = 1;
+        for (int i{a}; i < (b+1); i++) {
+            pr *= i;
+        }
+        std::cout << "from a to b " << pr << std::endl;
     }
-    std::cout << pr << std::endl;
 
 }
